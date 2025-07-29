7c717991003337d3ca755d3e6bd8343fe5628f7c
milanm5 <milanmiljkovic10012@gmail.com>
2025-07-29 21:49:43 +0200

    Modified Search Result service

---
+++ frontend/src/app/services/search-result.service.ts
@@ -0,0 +1,23 @@
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { SearchResult } from '../model/search-result';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+@Injectable({providedIn: "root"})
+export class SearchResultService {
+
+  private http = inject(HttpClient);
+  private api = "http://localhost:8080/search";
+  constructor() { 
+  }
+
+  public searchBooks(query: string): Observable<SearchResult> {
+    return this.http.get<SearchResult>(this.api, {
+      params: new HttpParams().append("q", query)
+    });
+  }
+
+}
