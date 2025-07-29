7a06fe0814f47b96e3f0254fb2930ce806420010
milanm5 <milanmiljkovic10012@gmail.com>
2025-07-29 21:54:43 +0200

    Added SearchResult route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { SearchResultComponent } from './components/search-result/search-result.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"searchResult", component: SearchResultComponent}
+];
