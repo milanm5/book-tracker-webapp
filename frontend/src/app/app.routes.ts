416e3bc6a63eeac62086b7a5ee7ba6de384d31f3
milanm5 <milanmiljkovic10012@gmail.com>
2026-02-16 22:23:59 +0100

    Added routes to login and register pages

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { SearchResultComponent } from './components/search-result/search-result.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
-    {path:"searchResult", component: SearchResultComponent}
+    {path:"searchResult", component: SearchResultComponent},
+    {path:"register", component: RegisterComponent},
+    {path:"login", component: LoginComponent}
 ];
