97c084e3f888350629661be0006a58ae25c8d958
milanm5 <milanmiljkovic10012@gmail.com>
2026-02-17 21:30:42 +0100

    Created user model

---
+++ backend/user-service/src/main/java/com/milanmiljkovic/model/User.java
@@ -0,0 +1,61 @@
+package com.milanmiljkovic.model;
+
+import java.io.Serializable;
+import jakarta.persistence.*;
+
+
+/**
+ * The persistent class for the user database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="User.findAll", query="SELECT u FROM User u")
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int idUser;
+
+	private String password;
+
+	private String username;
+	
+	private String email;
+
+	public User() {
+	}
+
+	public int getIdUser() {
+		return this.idUser;
+	}
+
+	public void setIdUser(int idUser) {
+		this.idUser = idUser;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getEmail() {
+		return this.email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
