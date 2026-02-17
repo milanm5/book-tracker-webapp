d2df57ebb0a8128ecaba6449d0cccd211b52e461
milanm5 <milanmiljkovic10012@gmail.com>
2026-02-17 22:07:02 +0100

    Created UserControl controller with register and login mappings

---
+++ backend/user-service/src/main/java/com/milanmiljkovic/control/UserControl.java
@@ -0,0 +1,37 @@
+package com.milanmiljkovic.control;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.milanmiljkovic.exception.EmailExistsException;
+import com.milanmiljkovic.exception.UsernameExistsException;
+import com.milanmiljkovic.model.User;
+import com.milanmiljkovic.service.UserService;
+
+@RestController
+public class UserControl {
+		
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping("/")
+	public String greeting() {
+		return "Hello World";
+	}
+
+	@PostMapping("/register")
+	public String register(@RequestBody User userToRegister) throws EmailExistsException, UsernameExistsException {
+			this.userService.register(userToRegister);		
+			return "Email already exists";
+	}
+	
+	@GetMapping("/login")
+	public String login(@RequestBody User userToLogin) {
+		
+		return "Successfully logged in";
+	}
+
+}
