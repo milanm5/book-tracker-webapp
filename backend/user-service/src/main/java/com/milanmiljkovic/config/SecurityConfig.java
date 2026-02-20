1e0b5789e2787de885d37bae264f04897d713e90
milanm5 <milanmiljkovic10012@gmail.com>
2026-02-21 00:38:00 +0100

    Added Authentication manager and provider beans

--- backend/user-service/src/main/java/com/milanmiljkovic/config/SecurityConfig.java
+++ backend/user-service/src/main/java/com/milanmiljkovic/config/SecurityConfig.java
@@ -2,6 +2,10 @@ package com.milanmiljkovic.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -14,33 +18,36 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
-	
+public class SecurityConfig{
+
 	@Bean
-	SecurityFilterChain securityFilterChain(HttpSecurity http) {
-		http
+	SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationProvider authenticationProvider) {
+		return http
 			.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll())
+			.authenticationProvider(authenticationProvider)
 			.csrf(csrf -> csrf.disable())
-			;
-		
-		
-			return http.build();
+			.build();		
 	}
 
 	@Bean
 	PasswordEncoder encoder() {
-		return new BCryptPasswordEncoder();
+		return new BCryptPasswordEncoder(12);
+	}    
+	
+	@Bean
+	AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+		return config.getAuthenticationManager();
 	}
-
-    @Bean
-    UserDetailsService userDetailsService(PasswordEncoder encoder) {
-		UserDetails user = User.builder()
-				.username("username")
-				.password(encoder.encode("ourpassowrd"))
-				.roles("ADMIN, USER")
-				.build();
-		return new InMemoryUserDetailsManager(user);
+	
+	@Bean
+	AuthenticationProvider authenticationProvider(
+			UserDetailsService userDetailsService, PasswordEncoder encoder) {
+		
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
+		provider.setPasswordEncoder(encoder);
+		return provider;
 	}
+    
 }
 
 
