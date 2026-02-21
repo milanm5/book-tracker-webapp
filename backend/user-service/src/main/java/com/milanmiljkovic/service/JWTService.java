fbe09947b77457ff6955d0b5cc95a1960e06684f
milanm5 <milanmiljkovic10012@gmail.com>
2026-02-22 00:32:04 +0100

    Added verification methods

--- backend/user-service/src/main/java/com/milanmiljkovic/service/JWTService.java
+++ backend/user-service/src/main/java/com/milanmiljkovic/service/JWTService.java
@@ -1,17 +1,18 @@
 package com.milanmiljkovic.service;
 
-import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
-
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -47,9 +48,45 @@ public class JWTService {
 				.compact();
 	}
 
-	private Key getKey() {
+	private SecretKey getKey() {
 		byte[] keyBytes = Decoders.BASE64.decode(secretKey);
 		return Keys.hmacShaKeyFor(keyBytes);
+	}
+
+
+	public String extractUsername(String token) {
+		return extractClaims(token, Claims::getSubject);
+	}
+
+
+	private <T> T extractClaims(String token, Function<Claims, T> claimResolver) {
+		final Claims claims = extractAllClaims(token);
+		return claimResolver.apply(claims);
+	}
+
+
+	private Claims extractAllClaims(String token) {
+		return Jwts.parser()
+				.verifyWith(getKey())
+				.build()
+				.parseSignedClaims(token)
+				.getPayload();
+	}
+
+
+	public boolean validateToken(String token, UserDetails userDetails) {
+		final String username = extractUsername(token);
+		return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+	}
+
+
+	private boolean isTokenExpired(String token) {
+		return extractExpiration(token).before(new Date());
+	}
+
+
+	private Date extractExpiration(String token) {
+		return extractClaims(token, Claims::getExpiration);
 	}
 
 }
