b92b6aa040b12929a82d6ab22c6fa061f96a1fb3
milanm5 <milanmiljkovic10012@gmail.com>
2026-02-22 00:31:28 +0100

    Added @Service annotation

--- backend/user-service/src/main/java/com/milanmiljkovic/service/CustomUserDetailsService.java
+++ backend/user-service/src/main/java/com/milanmiljkovic/service/CustomUserDetailsService.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.milanmiljkovic.model.User;
 import com.milanmiljkovic.repo.UserRepo;
 
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
 
 	@Autowired
