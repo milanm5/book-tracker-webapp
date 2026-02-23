1c357f2550431a9c6d0d6d987ee10f740b596e6d
milanm5 <milanmiljkovic10012@gmail.com>
2026-02-23 23:50:10 +0100

    Secret key is not being generated now every time

--- backend/user-service/src/main/java/com/milanmiljkovic/service/JWTService.java
+++ backend/user-service/src/main/java/com/milanmiljkovic/service/JWTService.java
@@ -1,15 +1,11 @@
 package com.milanmiljkovic.service;
 
-import java.security.NoSuchAlgorithmException;
-import java.util.Base64;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.function.Function;
 
-import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import io.jsonwebtoken.Claims;
@@ -20,30 +16,21 @@ import io.jsonwebtoken.security.Keys;
 @Service
 public class JWTService {
 	
+	@Value("${jwt.secret}")
 	private String secretKey;
 	
-	public JWTService() {
-		try {
-			KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
-			SecretKey sk = keyGen.generateKey();
-			secretKey = Base64.getEncoder().encodeToString(sk.getEncoded());
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		}
-		
+	@Value("${jwt.expiration}")
+	private int jwtExpiration;
+	
+	public JWTService() {		
 	}
 	
-	
 	public String generateToken(String username) {
-		Map<String, Object> claims = new HashMap<>();
 		
 		return Jwts.builder()
-				.claims()
-				.add(claims)
 				.subject(username)
 				.issuedAt(new Date(System.currentTimeMillis()))
-				.expiration(new Date(System.currentTimeMillis() * 60 * 60 * 30))
-				.and()
+				.expiration(new Date(System.currentTimeMillis() + this.jwtExpiration))
 				.signWith(getKey())
 				.compact();
 	}
