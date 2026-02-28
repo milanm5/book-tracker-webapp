bdb24a8e717d553425d12fbd65c5a9281a87f858
milanm5 <milanmiljkovic10012@gmail.com>
2026-02-28 23:53:31 +0100

    Added /login and /home routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { SearchResultComponent } from './components/search-result/search-result.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
     {path:"searchResult", component: SearchResultComponent},
     {path:"register", component: RegisterComponent},
-    {path:"login", component: LoginComponent}
+    {path:"login", component: LoginComponent},
+    {path:"home", component: HomeComponent}
 ];
