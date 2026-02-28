79f201a0797500c177ccd82b8a2d1d71832e7925
milanm5 <milanmiljkovic10012@gmail.com>
2026-02-28 23:48:14 +0100

    Moved request logic to BookService

---
+++ backend/book-service/src/main/java/com/milanmiljkovic/bookservice/controller/BookController.java
@@ -0,0 +1,27 @@
+package com.milanmiljkovic.bookservice.controller;
+
+import com.milanmiljkovic.bookservice.model.SearchResult;
+import com.milanmiljkovic.bookservice.service.BookService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class BookController {
+
+	@Autowired
+	private BookService bookService;
+	
+    @GetMapping("search")
+    public Mono<SearchResult> search(@RequestParam(name ="q") String searchQuery, @RequestParam int page) {
+
+        String query = searchQuery.replaceAll(" ", "+");
+
+        return this.bookService.search(query, page);
+    }
+
+
+}
---
+++ backend/book-service/src/main/java/com/milanmiljkovic/bookservice/service/BookService.java
@@ -0,0 +1,27 @@
+package com.milanmiljkovic.bookservice.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.milanmiljkovic.bookservice.model.SearchResult;
+
+import reactor.core.publisher.Mono;
+
+@Service
+public class BookService {
+
+	private final WebClient webClient;
+	
+	public BookService(WebClient.Builder webClientBuilder) {
+		this.webClient = webClientBuilder.baseUrl("http://openlibrary.org/search.json").build();
+	}
+	
+	public Mono<SearchResult> search(String query, int page) {
+		
+		return this.webClient.get().uri(uriBuilder -> uriBuilder
+				.queryParam("q", query)
+				.queryParam("offset", page * 5)
+				.queryParam("limit", 5)
+				.build()).retrieve().bodyToMono(SearchResult.class);
+	}
+}
