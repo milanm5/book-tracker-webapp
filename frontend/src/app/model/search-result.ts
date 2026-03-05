3fab44d194a96da2cdd87f18b4f361888900927e
milanm5 <milanmiljkovic10012@gmail.com>
2026-03-05 22:47:46 +0100

    Converted class to interface

--- frontend/src/app/model/search-result.ts
+++ frontend/src/app/model/search-result.ts
@@ -1,25 +1,6 @@
 import { Book } from "./book";
 
-export class SearchResult {
-
-    constructor(
-        public numFound: number,
-        public docs: Book[]
-    ) {}
-
-    public getNumFound() {
-        return this.numFound;
-    }
-
-    public getDocs() {
-        return this.getDocs;
-    }
-
-    public setNumFound(numFound: number) {
-        this.numFound = numFound;
-    }
-
-    public setDocs(docs: Book[]) {
-        this.docs = docs;
-    }
+export interface SearchResult {
+    numFound: number;
+    docs: Book[];
 }
