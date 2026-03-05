0247d8513d176a7d3e79a6943b855e8b61539de3
milanm5 <milanmiljkovic10012@gmail.com>
2026-03-05 22:48:45 +0100

    Converted Book to interface

--- frontend/src/app/model/book.ts
+++ frontend/src/app/model/book.ts
@@ -1,12 +1,9 @@
 import { Edition } from "./edition";
 
-export class Book {
-
-    constructor(
-        public key: string,
-        public title: string,
-        public author_name: string[],
-        public cover_i: string,
-        public editions: Edition
-    ) {}
+export interface Book {
+    key: string;
+    title: string;
+    author_name: string[];
+    cover_i: string;
+    editions: Edition;
 }
